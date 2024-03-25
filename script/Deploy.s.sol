777d43846e6ed5f48d17ddd531607820e713147d
ebok <shivdhandesh2002@gmail.com>
2024-03-25 19:24:30 +0530

    Added deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -8,10 +8,10 @@ import {Staking} from "src/Staking.sol";
 contract Deploy is Script {
     uint256 constant TOKEN_SUPPLY = 1000 ether;
     uint256 constant MINIMUM_STAKE_AMOUNT = 10 ether;
-    uint256 constant DEPLOYER = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+    uint256 constant DEPLOYER_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
     function run() public returns (DomToken, Staking) {
-        vm.startBroadcast(DEPLOYER);
+        vm.startBroadcast(DEPLOYER_PRIVATE_KEY);
         DomToken domToken = new DomToken("DomToken", "DOM", TOKEN_SUPPLY);
         Staking stake = new Staking(address(domToken), MINIMUM_STAKE_AMOUNT);
         vm.stopBroadcast();
