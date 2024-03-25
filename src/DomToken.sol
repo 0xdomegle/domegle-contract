1ac0bce52fc0d97bd74df7e0fe14b59fb14d78ae
ebok <shivdhandesh2002@gmail.com>
2024-03-25 19:22:22 +0530

    Fixed typo

--- src/DomToken.sol
+++ src/DomToken.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.24;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DomToken is ERC20 {
-    constructor(string memory name, string memory symbol, uint256 totalSupply) ERC20(name, symbol) {
-        _mint(msg.sender, totalSupply);
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) ERC20(_name, _symbol) {
+        _mint(msg.sender, _totalSupply);
     }
 }
